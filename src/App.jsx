ace6f071fe2f6abb5b42ef7c499dfb2a7590033d
salmanul-faris-k <sf102715@gmail.com>
2025-11-03 14:22:19 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,6 @@
 // App.jsx
-import React from 'react';
-import { PDFViewer } from '@react-pdf/renderer';
-import InvoiceDocument from './components/InvoiceDocument';
-import InvoiceGenerator from './components/InvoicePDF';
+
+import InvoiceGenerator from "./components/InvoicePDF";
 
 function App() {
   return (
